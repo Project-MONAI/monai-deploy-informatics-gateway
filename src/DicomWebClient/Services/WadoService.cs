90e8ecfd39d59ae69194cd3d8190e6189ae1aead
Victor Chang <vicchang@nvidia.com>
2022-03-14 10:37:31 -0700

    gh-65 address SonarQube issues
    Signed-off-by: Victor Chang <vicchang@nvidia.com>

--- src/DicomWebClient/Services/WadoService.cs
+++ src/DicomWebClient/Services/WadoService.cs
@@ -1,5 +1,5 @@
-// SPDX-FileCopyrightText: © 2021-2022 MONAI Consortium
-// SPDX-FileCopyrightText: © 2019-2020 NVIDIA Corporation
+// SPDX-FileCopyrightText: ï¿½ 2021-2022 MONAI Consortium
+// SPDX-FileCopyrightText: ï¿½ 2019-2020 NVIDIA Corporation
 // SPDX-License-Identifier: Apache License 2.0
 
 using System;
