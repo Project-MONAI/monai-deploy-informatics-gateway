5b4960015ac1cfa56703376146e3efd15282449a
Victor Chang <vicchang@nvidia.com>
2022-06-28 11:53:31 -0700

    Fix merge conflicts
    Signed-off-by: Victor Chang <vicchang@nvidia.com>

--- src/CLI/Commands/ConfigCommand.cs
+++ src/CLI/Commands/ConfigCommand.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.CommandLine;
-using System.CommandLine.Invocation;
 using System.CommandLine.NamingConventionBinder;
 using System.Threading;
 using System.Threading.Tasks;
