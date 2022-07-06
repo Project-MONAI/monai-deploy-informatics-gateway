a065d0e0383c3024b29ae7427f067ed3d8ab4113
Victor Chang <vicchang@nvidia.com>
2022-07-06 16:27:51 -0700

    Fix copyrights
    Signed-off-by: Victor Chang <vicchang@nvidia.com>

--- src/CLI/Test/ControlExceptionTest.cs
+++ src/CLI/Test/ControlExceptionTest.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿// SPDX-FileCopyrightText: © 2021-2022 MONAI Consortium
+// SPDX-License-Identifier: Apache License 2.0
+
+using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using Xunit;
 
--- src/InformaticsGateway/Services/Http/MonaiAeTitleController.cs
+++ src/InformaticsGateway/Services/Http/MonaiAeTitleController.cs
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: � 2021-2022 MONAI Consortium
+// SPDX-FileCopyrightText: © 2021-2022 MONAI Consortium
 // SPDX-License-Identifier: Apache License 2.0
 
 using System;
