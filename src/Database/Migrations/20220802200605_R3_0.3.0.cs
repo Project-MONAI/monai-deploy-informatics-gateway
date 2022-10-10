f076f3d4c0d8278ec55b243d7b0e4b03caa45077
Victor Chang <vicchang@nvidia.com>
2022-10-10 14:58:02 -0700

    Restore 0.3.0 database migration
    Signed-off-by: Victor Chang <vicchang@nvidia.com>

---
+++ src/Database/Migrations/20220802200605_R3_0.3.0.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * Copyright 2022 MONAI Consortium
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Monai.Deploy.InformaticsGateway.Database.Migrations
+{
+    public partial class R3_030 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StoragePath",
+                table: "InferenceRequest");
+
+            migrationBuilder.CreateTable(
+                name: "StorageMetadataWrapper",
+                columns: table => new
+                {
+                    CorrelationId = table.Column<string>(type: "TEXT", nullable: false),
+                    Identity = table.Column<string>(type: "TEXT", nullable: false),
+                    Value = table.Column<string>(type: "TEXT", nullable: true),
+                    TypeName = table.Column<string>(type: "TEXT", nullable: true),
+                    IsUploaded = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StorageMetadataWrapper", x => new { x.CorrelationId, x.Identity });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StorageMetadataWrapper");
+
+            migrationBuilder.AddColumn<string>(
+                name: "StoragePath",
+                table: "InferenceRequest",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
