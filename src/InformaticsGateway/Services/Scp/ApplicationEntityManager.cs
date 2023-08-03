c674ab8a0ba99ab4680ea846e91e4b7f10bba1c7
Victor Chang <vicchang@nvidia.com>
2023-08-03 15:18:36 -0700

    Update fo-dicom
    Signed-off-by: Victor Chang <vicchang@nvidia.com>

--- src/InformaticsGateway/Services/Scp/ApplicationEntityManager.cs
+++ src/InformaticsGateway/Services/Scp/ApplicationEntityManager.cs
@@ -18,7 +18,6 @@
 using System;
 using System.Collections.Concurrent;
 using System.Threading.Tasks;
-using Amazon.Runtime.Internal;
 using Ardalis.GuardClauses;
 using FellowOakDicom.Network;
 using Microsoft.Extensions.DependencyInjection;
