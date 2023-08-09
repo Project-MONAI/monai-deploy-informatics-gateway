154a62366d09cbacf7dfb0f9947d9ef21bf30078
Neil South <neil.south@answerdigital.com>
2023-08-09 11:03:07 +0100

    fixing up errors
    Signed-off-by: Neil South <neil.south@answerdigital.com>
    Signed-off-by: Victor Chang <vicchang@nvidia.com>

--- src/Database/EntityFramework/Repositories/RemoteAppExecutionRepository.cs
+++ src/Database/EntityFramework/Repositories/RemoteAppExecutionRepository.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2022 MONAI Consortium
+ * Copyright 2021-2023 MONAI Consortium
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 
 using Ardalis.GuardClauses;
 using Microsoft.EntityFrameworkCore;
