f612ab38b4a42a5c18608dba6c4d337b1fbb2953
Sam Rooke <sam.rooke@answerdigital.com>
2023-09-12 15:44:29 +0100

    add migration for tenant id

---
+++ src/Database/EntityFramework/Migrations/20230912144216_R_0.4.2.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Monai.Deploy.InformaticsGateway.Database.Migrations
+{
+    public partial class R_042 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "TenantId",
+                table: "SourceApplicationEntities",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "TenantId",
+                table: "MonaiApplicationEntities",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "TenantId",
+                table: "DestinationApplicationEntities",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                table: "SourceApplicationEntities");
+
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                table: "MonaiApplicationEntities");
+
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                table: "DestinationApplicationEntities");
+        }
+    }
+}
