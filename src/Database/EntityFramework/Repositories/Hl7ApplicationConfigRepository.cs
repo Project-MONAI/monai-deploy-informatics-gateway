95e4c7d68b36bcda0fdebaa13ee5322bd6e1e736
Lillie Dae <lillie.dae@answerdigital.com>
2023-11-16 16:39:34 +0000

    changes for HL7Configuration endpoint
    Signed-off-by: Lillie Dae <lillie.dae@answerdigital.com>

--- src/Database/EntityFramework/Repositories/Hl7ApplicationConfigRepository.cs
+++ src/Database/EntityFramework/Repositories/Hl7ApplicationConfigRepository.cs
@@ -21,6 +21,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Monai.Deploy.InformaticsGateway.Api;
 using Monai.Deploy.InformaticsGateway.Configuration;
+using Monai.Deploy.InformaticsGateway.Database.Api;
 using Monai.Deploy.InformaticsGateway.Database.Api.Logging;
 using Monai.Deploy.InformaticsGateway.Database.Api.Repositories;
 using Polly;
@@ -63,6 +64,10 @@ namespace Monai.Deploy.InformaticsGateway.Database.EntityFramework.Repositories
             return _retryPolicy.ExecuteAsync(async () =>
             {
                 var entity = await GetByIdAsync(id).ConfigureAwait(false);
+                if (entity is null)
+                {
+                    throw new DatabaseException("Failed to delete entity.");
+                }
                 var result = _dataset.Remove(entity);
                 await _informaticsGatewayContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
                 return result.Entity;
