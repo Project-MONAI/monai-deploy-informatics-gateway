b1f6c491389c538dbf05749f0ddb88d41023f9c1
Lillie Dae <lillie.dae@answerdigital.com>
2023-11-20 09:10:31 +0000

    minor fix
    Signed-off-by: Lillie Dae <lillie.dae@answerdigital.com>

--- src/Database/EntityFramework/Configuration/Hl7ApplicationConfigConfiguration.cs
+++ src/Database/EntityFramework/Configuration/Hl7ApplicationConfigConfiguration.cs
@@ -25,11 +25,9 @@ namespace Monai.Deploy.InformaticsGateway.Database.EntityFramework.Configuration
         public void Configure(EntityTypeBuilder<Hl7ApplicationConfigEntity> builder)
         {
             builder.HasKey(j => j.Id);
-            builder.Property(j => j.DataLink.Key).IsRequired();
-            builder.Property(j => j.DataLink.Value).IsRequired();
+            builder.Property(j => j.DataLink).IsRequired();
             builder.Property(j => j.DataMapping).IsRequired();
-            builder.Property(j => j.SendingId.Key).IsRequired();
-            builder.Property(j => j.SendingId.Value).IsRequired();
+            builder.Property(j => j.SendingId).IsRequired();
 
             builder.Ignore(p => p.Id);
         }
